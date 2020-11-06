9e4849b92a6c8fd74dedf2371c16a997f5d839f8
Manvi Madan <manvimadan@outlook.com>
2020-11-06 21:21:00 +1300

    object detection project added

---
+++ Object Detection/Object+Detection.py
@@ -0,0 +1,16 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+# Object Detection
+
+# Importing the libraries
+import torch
+from torch.autograd import Variable
+import cv2
+from data import BaseTransform, VOC_CLASSES as labelmap
+from ssd import build_ssd
+import imageio
+
